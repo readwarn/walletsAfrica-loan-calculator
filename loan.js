88bf4b31c2a6775a3c96db748c93a6f1cfc547a9
Allams <anthony.allams@gmail.com>
2020-06-18 20:35:16 +0100

    Removed subpages

--- loan.js
+++
@@ -1,7 +0,0 @@
-var navlinks=$(".collapse")
-$(".close").click(function (e) { 
-    navlinks.toggleClass("hidden");
-    $(".l1").toggleClass("line-1");
-    $(".l2").toggleClass("line-2");
-    $(".l3").toggleClass("line-3");
-});
