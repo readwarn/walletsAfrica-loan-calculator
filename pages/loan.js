2bfd18feb82157b9e33cc08bc0de8bc1462f3523
Yusuf Rilwan <akinyusuf5@gmail.com>
2020-06-19 09:06:08 +0100

    js rounded up

--- pages/loan.js
+++ pages/loan.js
@@ -19,18 +19,26 @@ var tc=$(".t-cancel");
 var alert1=$(".alert");
 var duration=$("#duration");
 var loan=$("#loan");
+var interest=$("#interest");
 
 calc.click(function(e){
-
-      var loan1=parseInt(loan.val());
-      var duration1=parseInt(duration.val());
-    if(isNaN(loan1)||isNaN(duration1)){
+      var principal=parseFloat(loan.val());
+      var interest1=parseFloat(interest.val()) / 100 / 12;
+      var payments=parseFloat(duration.val()) * 12;
+      //Monthly payment
+      var x = Math.pow(1 + interest1, payments); 
+      var monthly = (principal*x*interest1)/(x-1);
+       
+     //if user input is valid
+    if(!isFinite(monthly)){
        alert1.removeClass("hidden");
     }
     else{
+        monthlyPay.val(monthly.toFixed(2));
+        totalPayment.val((monthly*payments).toFixed(2));
+        totalInterest.val(((monthly*payments)-principal).toFixed(2));
         output.removeClass("hidden");
         output.addClass("animate__slideInRight");
-
     }
 });
 cancel.click(function(e){
